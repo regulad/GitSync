7a5fc3178f07b7df68766f540dbecd4d853e8251
Parker Wahle <regulad@regulad.xyz>
2022-07-10 14:38:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# PaperTemplate
+
+A quick template for making shitty plugins.
+
+**Depreciated. Please see [`regulad/PaperGradleTemplate`](https://github.com/regulad/PaperGradleTemplate).**
---
+++ src/main/java/xyz/regulad/papertemplate/PaperTemplate.java
@@ -0,0 +1,10 @@
+package xyz.regulad.papertemplate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PaperTemplate extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        this.getLogger().info("Loaded template plugin!");
+    }
+}
